3d1355ed7be090dcd829d7bd9d0475b630e23a9e
poornachandra kashi <44542685+poornachandrakashi@users.noreply.github.com>
2020-05-17 02:51:11 +0530

    dhgfh

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+#Learning Flask application.
+
 from flask import Flask
 
 app=Flask(__name__)
@@ -5,6 +7,13 @@ app=Flask(__name__)
 @app.route("/")   #Empty url
 def index():
     return "HEllo,world!!!"
+
+@app.route("/<string:name>")
+def hello(name):
+    name=name.capitalize()
+    return f"<h1>Hello {name}!</h1>"
+
+
 
 
 if __name__=='__main__':
